eb13d271c89621365a069d5031c855c31c5f9254
Michelle <michelle.npm3108@gmail.com>
2021-01-12 05:44:56 +0800

    move to next slide

--- index.js
+++ index.js
@@ -3,15 +3,24 @@ const slides = document.getElementsByClassName('carousel-item');
 document.querySelector('.carousel-btn-prev').addEventListener('click', moveToPrevSlide)
 document.querySelector('.carousel-btn-next').addEventListener('click', moveToNextSlide)
 
-let i=0;
-const totalSlides = slides.length
-function moveToNextSlide(){
-    if (i === totalSlides-1) {
-        i=0
-    } else { 
-        i++
+function hideAllSlides() {
+    for(let slide of slides) {
+        slide.classList.add('carousel-item-hidden')
     }
-slides[i].classList.add('carousel-item-visible')    
+}
+
+
+let slidePosition = 0;
+const totalSlides = slides.length;
+function moveToNextSlide(){
+
+    if (slidePosition === totalSlides-1) {
+      slidePosition = 0;
+  } else {
+      slidePosition ++;
+  }
+  hideAllSlides()
+  slides[slidePosition].classList.add('carousel-item-visible')
 }
 
 function moveToPrevSlide(){
